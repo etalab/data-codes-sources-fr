9dbeb94507a14c1a914efcc55fd905f4d89513ee
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-01-28 10:27:12 +0100

    Auto format Python

--- github.py
+++ github.py
@@ -3,39 +3,38 @@ import requests
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
-GITHUB_API_ROOT = 'https://api.github.com/'
+GITHUB_API_ROOT = "https://api.github.com/"
 
-retries = Retry(
-    total=5,
-    backoff_factor=0.1,
-    status_forcelist=[500, 502, 503, 504])
+retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
 
-requests.Session().mount('https://', HTTPAdapter(max_retries=retries))
+requests.Session().mount("https://", HTTPAdapter(max_retries=retries))
 
 
 def github_headers():
     return {
-        'Authorization': 'token ' + os.getenv('GITHUB_TOKEN'),
-        'Accept': ';'.join([
-            'application/vnd.github.v3+json',
-            'application/vnd.github.mercy-preview+json'
-        ]),
-        'User-Agent': 'AntoineAugusti'
+        "Authorization": "token " + os.getenv("GITHUB_TOKEN"),
+        "Accept": ";".join(
+            [
+                "application/vnd.github.v3+json",
+                "application/vnd.github.mercy-preview+json",
+            ]
+        ),
+        "User-Agent": "AntoineAugusti",
     }
 
 
 def repos_for_org(organisation):
     data = []
 
-    base_url = GITHUB_API_ROOT + 'orgs/' + organisation + '/repos?per_page=100'
+    base_url = GITHUB_API_ROOT + "orgs/" + organisation + "/repos?per_page=100"
     response = requests.get(base_url, headers=github_headers())
     if response.status_code == 404:
         return []
     response.raise_for_status()
     data.extend(response.json())
 
-    while 'next' in response.links:
-        url = response.links['next']['url']
+    while "next" in response.links:
+        url = response.links["next"]["url"]
         response = requests.get(url, headers=github_headers())
         data.extend(response.json())
 
@@ -43,7 +42,7 @@ def repos_for_org(organisation):
 
 
 def get_org(organisation):
-    base_url = GITHUB_API_ROOT + 'orgs/' + organisation
+    base_url = GITHUB_API_ROOT + "orgs/" + organisation
 
     response = requests.get(base_url, headers=github_headers())
     if response.status_code == 404:
--- storage.py
+++ storage.py
@@ -1,45 +1,43 @@
 import csv
 import json
 
-ORGS_FOLDER = 'data/organisations/'
-REPOS_FOLDER = 'data/repertoires/'
+ORGS_FOLDER = "data/organisations/"
+REPOS_FOLDER = "data/repertoires/"
 
 
 def filename(organisation, mode, extension):
-    if mode == 'repo':
+    if mode == "repo":
         folder = REPOS_FOLDER
-    elif mode == 'org':
+    elif mode == "org":
         folder = ORGS_FOLDER
     else:
         raise ValueError
-    if extension not in ['csv', 'json']:
+    if extension not in ["csv", "json"]:
         raise ValueError
 
-    return '{folder}{extension}/{organisation}.{extension}'.format(
-        folder=folder,
-        extension=extension,
-        organisation=organisation
+    return "{folder}{extension}/{organisation}.{extension}".format(
+        folder=folder, extension=extension, organisation=organisation
     )
 
 
 def save_data(organisation, data, mode):
     # Save in CSV
-    with open(filename(organisation, mode, 'csv'), 'w') as f:
+    with open(filename(organisation, mode, "csv"), "w") as f:
         w = csv.writer(f)
         w.writerow(data.keys())
         w.writerows(zip(*data.values()))
 
     # Save in JSON
-    with open(filename(organisation, mode, 'json'), 'w') as f:
+    with open(filename(organisation, mode, "json"), "w") as f:
         data = [dict(zip(data.keys(), i)) for i in zip(*data.values())]
-        if mode == 'org' and len(data) == 1:
+        if mode == "org" and len(data) == 1:
             data = data[0]
         json.dump(data, f, ensure_ascii=False)
 
 
 def save_repos_for_org(organisation, data):
-    save_data(organisation, data, 'repo')
+    save_data(organisation, data, "repo")
 
 
 def save_org(organisation, data):
-    save_data(organisation, data, 'org')
+    save_data(organisation, data, "org")
