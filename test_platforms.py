8114159a78011d25e904fb5580c5ff19de2cd6d1
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-08-09 12:20:56 +0200

    Add a detector class for platforms

---
+++ test_platforms.py
@@ -0,0 +1,51 @@
+import unittest
+
+from platforms import Detector
+from github import GitHubOrg
+from gitlab import GitLabOrg
+
+
+class TestPlatforms(unittest.TestCase):
+    def setUp(self):
+        self.detector = Detector()
+
+    def test_find_domain(self):
+        self.assertEquals(
+            self.detector.find_domain("https://github.com/etalab"), "github.com"
+        )
+
+        with self.assertRaises(ValueError):
+            self.detector.find_domain("http://example.fr")
+
+    def test_find_platform(self):
+        self.assertEquals(
+            self.detector.find_platform("https://github.com/etalab"), "github"
+        )
+
+        with self.assertRaises(ValueError):
+            self.detector.find_platform("http://example.fr")
+
+        with self.assertRaises(NotImplementedError):
+            self.detector.find_platform("http://sourcesup.renater.fr")
+
+    def test_platform_base_url(self):
+        self.assertEquals(
+            self.detector.platform_base_url("https://github.com/etalab"),
+            "https://api.github.com/",
+        )
+        self.assertEquals(
+            self.detector.platform_base_url("https://gitlab.com/pidila"),
+            "https://gitlab.com/api/v4/",
+        )
+
+    def test_to_org(self):
+        org = self.detector.to_org("https://github.com/etalab/")
+
+        self.assertIsInstance(org, GitHubOrg)
+        self.assertEquals(org.organisation, "etalab")
+        self.assertEquals(org.base_url, "https://api.github.com/")
+
+        org = self.detector.to_org("https://framagit.org/etalab")
+        self.assertIsInstance(org, GitLabOrg)
+        self.assertEquals(org.organisation, "etalab")
+        self.assertEquals(org.base_url, "https://framagit.org/api/v4/")
