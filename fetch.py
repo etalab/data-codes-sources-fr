708f4f019fecef52366c2765da572ca716fe6b95
Guillim <guigloo@msn.com>
2021-09-17 16:05:56 +0200

    adding the ability to save packages

--- fetch.py
+++ fetch.py
@@ -3,25 +3,36 @@ from io import BytesIO
 from urllib.request import urlopen
 
 from platforms import Detector as PlatformDetector
-from storage import save_repos, save_orgs
+from storage import save_repos, save_orgs, save_packages
 
 
 def fetch_orgs(detector):
     organisations = []
 
-    resp = urlopen(
-        "https://raw.githubusercontent.com/DISIC/politique-de-contribution-open-source/master/comptes-organismes-publics"
-    )
-    data = BytesIO(resp.read())
-    data = set([l.lower().decode("utf-8").strip() for l in data])
+    # resp = urlopen(
+    #     "https://raw.githubusercontent.com/DISIC/politique-de-contribution-open-source/master/comptes-organismes-publics"
+    # )
+
+    # data = BytesIO(resp.read())
+    # data = set([l.lower().decode("utf-8").strip() for l in data])
+    data = [
+        "https://git.unicaen.fr/pdn-certic",
+        "https://github.com/1024pix",
+        "https://github.com/139bercy",
+        "https://github.com/ansforge",
+    ]
+    
+    data = set([l.lower().strip() for l in data])
 
     for line in data:
         try:
             organisations.extend(detector.to_orgs(line))
         except Exception as e:
             print(e)
-
+    print(organisations)
     return organisations
+    
+
 
 
 detector = PlatformDetector()
@@ -38,6 +49,11 @@ for organisation in organisations:
         all_repos[k].extend(v)
 
 save_repos(all_repos)
+
+# Save packages created by each org
+all_packs = all_repos
+
+save_packages(all_repos)
 
 # Save details about each org
 all_orgs = defaultdict(list)
